ccb9e53c259190e0fb33021ec7715ed2a10e47bc
Joao Pellacani <jaum.gui001@gmail.com>
2024-10-16 20:38:58 -0300

    Update

---
+++ bee1038.py
@@ -0,0 +1,27 @@
+'''
+Com base na tabela abaixo, escreva um programa que leia o código de um item e a quantidade deste item. A seguir, calcule e mostre o valor da conta a pagar.
+
+
+
+Entrada
+O arquivo de entrada contém dois valores inteiros correspondentes ao código e à quantidade de um item conforme tabela acima.
+
+Saída
+O arquivo de saída deve conter a mensagem "Total: R$ " seguido pelo valor a ser pago, com 2 casas após o ponto decimal.
+'''
+codigo, quantidade = map(int, input().split())
+
+valor = 0.0
+
+if codigo == 1:
+    valor = 4 * quantidade
+elif codigo == 2:
+    valor = 4.50 * quantidade
+elif codigo == 3:
+    valor = 5 * quantidade
+elif codigo == 4:
+    valor = 2 * quantidade
+elif codigo == 5:
+    valor = 1.5 * quantidade
+
+print(f'Total: R$ {valor:.2f}')
---
+++ bee1042.py
@@ -0,0 +1,18 @@
+'''
+Leia 3 valores inteiros e ordene-os em ordem crescente. 
+No final, mostre os valores em ordem crescente, uma linha em branco e em seguida, os valores na sequência como foram lidos.
+
+Entrada
+A entrada contem três números inteiros.
+
+Saída
+Imprima a saída conforme foi especificado.
+'''
+valor_int = map(int, input())
+
+print(valor_int)
+# print()
+# valor_crescente = sorted(valor_int)
+# print(valor_crescente)
+
+# Parei aqui
