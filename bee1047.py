211c8e4d72076362f1cfd90c5693739136c1a350
João Guilherme <eng.joaopellacani@gmail.com>
2024-10-23 12:52:34 -0300

    Bee1047

--- bee1047.py
+++ bee1047.py
@@ -9,27 +9,19 @@ Quatro números inteiros representando a hora de início e fim do jogo.
 Saída
 Mostre a seguinte mensagem: “O JOGO DUROU XXX HORA(S) E YYY MINUTO(S)” .
 '''
-def calculo_horas(hora_inicial, hora_final):
-    if hora_inicial < hora_final:
-        duracao = hora_final - hora_inicial
-        return duracao
+def calculo_minutos_horas(hora_inicial, minuto_inicial, hora_final, minuto_final):
+    minutos_iniciais = hora_inicial * 60 + minuto_inicial
+    minutos_finais = hora_final * 60 + minuto_final
+    if minutos_iniciais < minutos_finais:
+        duracao = minutos_finais - minutos_iniciais
     else:
-        duracao = (24 - hora_inicial) + hora_final
-        return duracao
-
-def calculo_minutos(minuto_inicial, minuto_final):
-    if minuto_inicial < minuto_final:
-        duracao = minuto_final - minuto_inicial
-        return duracao
-    elif minuto_inicial == minuto_final:
-        return 0
-    else:
-        duracao = 60 - (minuto_inicial - minuto_final)
-        return duracao
+        duracao = 24 * 60 - minutos_iniciais + minutos_finais
+    horas = duracao // 60
+    minutos = duracao % 60
+    return horas, minutos
 
 hora_i, minuto_i, hora_f, minuto_f = map(int, input().split())
-retorno_horas = calculo_horas(hora_i, hora_f)
-retorno_minutos = calculo_minutos(minuto_i, minuto_f)
-print(f'O JOGO DUROU {retorno_horas} HORA(S) E {retorno_minutos} MINUTO(S)')
+horas, minutos = calculo_minutos_horas(hora_i, minuto_i, hora_f, minuto_f)
+print(f'O JOGO DUROU {horas} HORA(S) E {minutos} MINUTO(S)')
 
 # Corrigir erro quando da 59 minutos e aparece 1H, entrada exemplo 7 10 8 9
