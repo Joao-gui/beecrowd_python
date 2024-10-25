2cca8d3d81d86b35312a9242ac15ad73bff9304c
João Guilherme <eng.joaopellacani@gmail.com>
2024-10-25 11:10:56 -0300

    bee1059

---
+++ bee1059.py
@@ -0,0 +1,13 @@
+'''
+Faça um programa que mostre os números pares entre 1 e 100, inclusive.
+
+Entrada
+Neste problema extremamente simples de repetição não há entrada.
+
+Saída
+Imprima todos os números pares entre 1 e 100, inclusive se for o caso, um em cada linha.
+'''
+numero = range(1, 101)
+for i in numero:
+    if i % 2 == 0:
+        print(i)
