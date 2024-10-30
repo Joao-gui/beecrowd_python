7568bb4d294e1405c63c96e70220a81ce20913d1
João Guilherme <eng.joaopellacani@gmail.com>
2024-10-30 11:04:24 -0300

    bee1075

---
+++ bee1075.py
@@ -0,0 +1,15 @@
+'''
+Leia um valor inteiro N. Apresente todos os números entre 1 e 10000 que divididos por N dão resto igual a 2.
+
+Entrada
+A entrada contém um valor inteiro N (N < 10000).
+
+Saída
+Imprima todos valores que quando divididos por N dão resto = 2, um por linha.
+'''
+n = int(input())
+
+if n < 10000:
+    for i in range(10000):
+        if i % n == 2:
+            print(i)
