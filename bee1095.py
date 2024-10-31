dc8835b315b5dd85038d8547e8e39286e13f4809
João Guilherme <eng.joaopellacani@gmail.com>
2024-10-31 14:54:27 -0300

    bee1095

---
+++ bee1095.py
@@ -0,0 +1,16 @@
+'''
+Você deve fazer um programa que apresente a sequencia conforme o exemplo abaixo.
+
+Entrada
+Não há nenhuma entrada neste problema.
+
+Saída
+Imprima a sequencia conforme exemplo abaixo
+'''
+i = 1
+j = 60
+
+while (j >= 0):
+    print(f'I={i} J={j}')
+    i += 3
+    j -= 5
