6ce8e43c44abf838403b4119f6f5e8e4d6403b79
João Guilherme <eng.joaopellacani@gmail.com>
2024-11-05 11:20:37 -0300

    bee1113

---
+++ bee1113.py
@@ -0,0 +1,20 @@
+'''
+Leia uma quantidade indeterminada de duplas de valores inteiros X e Y. 
+Escreva para cada X e Y uma mensagem que indique se estes valores foram digitados em ordem crescente ou decrescente.
+
+Entrada
+A entrada contém vários casos de teste. Cada caso contém dois valores inteiros X e Y. 
+A leitura deve ser encerrada ao ser fornecido valores iguais para X e Y.
+
+Saída
+Para cada caso de teste imprima “Crescente”, caso os valores tenham sido digitados na ordem crescente, 
+caso contrário imprima a mensagem “Decrescente”.
+'''
+x, y = map(int, input().split())
+
+while(x > 0 and y > 0 and x != y):
+    if x > y:
+        print('Decrescente')
+    elif y > x:
+        print('Crescente')
+    x, y = map(int, input().split())
