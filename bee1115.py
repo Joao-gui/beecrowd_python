dfbb7d77277c8a3df636ccf25aab6447593339ed
João Guilherme <eng.joaopellacani@gmail.com>
2024-11-05 11:33:21 -0300

    bee1115

---
+++ bee1115.py
@@ -0,0 +1,27 @@
+'''
+Escreva um programa para ler as coordenadas (X,Y) de uma quantidade indeterminada de pontos no sistema cartesiano. 
+Para cada ponto escrever o quadrante a que ele pertence. 
+O algoritmo será encerrado quando pelo menos uma de duas coordenadas for NULA (nesta situação sem escrever mensagem alguma).
+
+Entrada
+A entrada contém vários casos de teste. Cada caso de teste contém 2 valores inteiros.
+
+Saída
+Para cada caso de teste mostre em qual quadrante do sistema cartesiano se encontra a coordenada lida, conforme o exemplo.
+'''
+cond = 0
+
+while(cond == 0):
+    x, y = map(int, input().split())
+    if x > 0:
+        if y > 0:
+            print('primeiro')
+        elif y < 0:
+            print('quarto')
+    elif x < 0:
+        if y > 0:
+            print('segundo')
+        elif y < 0:
+            print('terceiro')
+    else:
+        cond = 1
